ef1492a07e547601f224b0c0d9cf3ebf357189f3
danielgavrila2 <danielgavrila2@gmail.com>
2026-03-22 22:32:13 +0200

    feat/backend: added authentication

---
+++ Assignment_1/MVC-App/src/main/java/com/sd/enterprise/mvcapp/dto/AuthResponse.java
@@ -0,0 +1,20 @@
+package com.sd.enterprise.mvcapp.dto;
+
+import com.sd.enterprise.mvcapp.entity.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+    private String token;
+    private String type = "Bearer";
+    private Long userId;
+    private String email;
+    private String fullName;
+    private Role role;
+}
